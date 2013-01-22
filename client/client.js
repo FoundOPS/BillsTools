ed3e848b6cc0a0e17ac8bb44b61037949a78405e
apohl1111 <apohl@foundops.com>
2013-01-22 17:01:44 -0500

    setup logout

--- client/client.js
+++ client/client.js
@@ -97,9 +97,13 @@ Template.topBar.rendered = function () {
     $("#menu").optionsPopup({
         id: "menu",
         contents: [
-            {name: "Logout"}
+            {name: "Logout", id: "logout"}
         ],
         disableHeader: true
+    });
+
+    $("#logout").live("click", function (e) {
+        Meteor.logout();
     });
 };
 
