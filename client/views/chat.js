847b1431c52ba83420439a4eab87cecfdb136c81
apohl1111 <apohl@foundops.com>
2013-01-25 15:26:32 -0500

    start setup teams page

--- client/views/chat.js
+++ client/views/chat.js
@@ -221,6 +221,16 @@ Template.chat.destroyed = function () {
 ///////////////////////////////////////////////////////////////////////////////
 // Initialization
 
+$(document).on("mobileinit", function () {
+    // Reference: http://jquerymobile.com/demos/1.1.0/docs/api/globalconfig.html
+    $.extend($.mobile, {
+        linkBindingEnabled: false,
+        ajaxEnabled: false
+    });
+});
+
+//TODO: why is .ui-loader showing up(now it's just being hidden in css)
+
 Meteor.startup(function () {
     setupMobileChat();
 });
---
+++ client/views/team.js
@@ -0,0 +1,18 @@
+Template.teamView.rendered = function () {
+
+};
+
+Template.grid.members = [
+    {
+        Name: "Bob Brown",
+        Email: "bbrown@gmail.com",
+        Role: "member",
+        Status: "Invited"
+    },
+    {
+        Name: "James Peach",
+        Email: "peachyclean@gmail.com",
+        Role: "admin",
+        Status: "Last Login: 11:42am"
+    }
+];
