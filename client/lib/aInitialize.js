cad4e65989af372f849c69f4500a6197dc539164
jperl <jperl@foundops.com>
2013-01-30 11:21:32 -0500

    setup jquery mobile pages

--- client/lib/aInitialize.js
+++ client/lib/aInitialize.js
@@ -2,12 +2,7 @@
 //http://stackoverflow.com/questions/10693113/dependencies-between-javascript-files-in-meteor
 
 $(document).on("mobileinit", function () {
-    //disable jQuery page stuff
-    $.mobile.ajaxEnabled = false;
-    $.mobile.autoInitializePage = false;
-    $.mobile.pushStateEnabled = false;
-
-    //required for many listeners (popup closing when you click outside of it)
-    //if not set and there is not a first page, there will be errors
-    $.mobile.pageContainer = $(document.body).addClass("ui-mobile-viewport");
+    //disable url listening. use change page manually
+    $.mobile.linkBindingEnabled = false;
+    $.mobile.hashListeningEnabled = false;
 });
